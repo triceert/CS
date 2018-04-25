e530c338031dde26d2ee01428a9ee42b1d48b5f1
Clemens Isert <clemens.isert@googlemail.com>
2018-04-25 09:47:10 +0200

    implementation of hcn_distillation into main

--- MatlabWD/hcn_distillation.m
+++ MatlabWD/hcn_distillation.m
@@ -1,4 +1,4 @@
-function [cmpout, untout, strout] = hcn_distillation(cmpin, untin, strin)
+function [cmpout, untout, strout] = hcn_distillation(cmpin, untin, strin, thermo_model)
 % Calculation of HCN distillation using the Fenske-Underwood-Gilliland
 % method
 % since we are at bubble point (Aufgabenstellung: "führen Sie den Feed im Siedezustand zu"), there's only L coming
@@ -9,43 +9,23 @@ function [cmpout, untout, strout] = hcn_distillation(cmpin, untin, strin)
 %%%%%%%%%%%%%%%%
 
 feed_L = strin(9).L 
-feed_V = strin(9).G; % molar flowrate in feed [mol/hr]
-
-q = 1; % feed quality (fraction of feed that is liquid, q=1 since @ bubble point)
+feed_V = strin(9).G;                                                    % molar flowrate in feed [mol/hr]
+q = 1;                                                                  % feed quality (fraction of feed that is liquid, q=1 since @ bubble point)
 z.H2O = (strin(9).xH2O*feed_L + strin(9).yH2O *feed_V)/(feed_L+feed_V); 
-%z.CH4 = (strin(9).xCH4*feed_L + strin(9).yCH4 *feed_V)/(feed_L+feed_V); 
-%z.NH3 = (strin(9).xNH3*feed_L + strin(9).yNH3 *feed_V)/(feed_L+feed_V);  
-%z.Egas = (strin(9).xEgas*feed_L + strin(9).yEgas *feed_V)/(feed_L+feed_V);  
-%z.H2SO4 = (strin(9).xH2SO4*feed_L + strin(9).yH2SO4 *feed_V)/(feed_L+feed_V);  
 z.HCN = (strin(9).xHCN*feed_L + strin(9).yHCN *feed_V)/(feed_L+feed_V); 
 %z.AS = (strin(9).xAS*feed_L + strin(9).yAS *feed_V)/(feed_L+feed_V);  
 %z.H2 = (strin(9).xH2*feed_L + strin(9).yH2 *feed_V)/(feed_L+feed_V); 
 %F = feed_L + feed_V; % since the whole outlet stream of the HCN absorption unit will be condensed before entering the column
 F = 100; % mol/hr, just some value to try calculations
-MW_H2O = 0.018; 
-MW_HCN = 0.02703; 
-thermo_model = 'nrtl'; 
+MW_H2O = cmpin(1).MW; 
+MW_HCN = cmpin(6).MW;
+thermo_model = 'nrtl';  
 
-A_CH4 = 6.7021; 	% Antoine parameters from http://ddbonline.ddbst.com/AntoineCalculation/AntoineCalculationCGI.exe?component=Methane 
-B_CH4 = 394.48; 	 
-C_CH4 = 264.609;
-
-A_NH3 = 4.86886;   % https://webbook.nist.gov/cgi/cbook.cgi?ID=C7664417&Mask=4&Type=ANTOINE&Plot=on#ANTOINE
-B_NH3 = 1113.928;
-C_NH3 = -10.409; 
-
-A_H2O = 7.96681; B_H2O = 1668.21; C_H2O = 228.0; % (values for T > 60 °C), from Excel file  
-A_HCN = 7.5282; B_HCN = 1329.5; C_HCN = 260.4;      % from Excel file
+A_H2O = cmpin(1).anta100; B_H2O = cmpin(1).antb100; C_H2O = cmpin(1).antc100; 
+A_HCN = cmpin(6).anta100; B_HCN = cmpin(6).antb100; C_HCN = cmpin(6).antc100; 
 
 P_sat_H2O = @(temperature) antoine_equation(A_H2O, B_H2O, C_H2O, temperature);
-P_sat_CH4 = @(temperature) antoine_equation(A_CH4, B_CH4, C_CH4, temperature); 
-P_sat_NH3 = @(temperature) antoine_equation(A_NH3, B_NH3, C_NH3, temperature); 
-P_sat_Egas = @(temperature) 10^10; 
-P_sat_H2SO4 = @(temperature) 10^10;
 P_sat_HCN = @(temperature) antoine_equation(A_HCN, B_HCN, C_HCN, temperature);
-P_sat_AS = @(temperature) 10^10;
-P_sat_H2 = @(temperature) 10^10;
-
 
 x_LK_D = 0.995;         % from specifications on tasksheet
 x_HK_D = 1-x_LK_D; 
@@ -53,18 +33,18 @@ x_LK_B = 10e-6;
 x_HK_B = 1-x_LK_B;
 
 
-T_boiling_H2O = 373.15; 
-T_boiling_HCN = 299; % from Wikipedia, [K]
+T_boiling_H2O = cmpin.bp(1); 
+T_boiling_HCN = cmpin.bp(6);  
 %T_boiling_mixture_assumption = 373.15*0.8+299*0.2; % just a shitty weighted average, no thermodynamics here --> use feed temperature once available 
-T_boiling_mixture_assumption = 317.17;
-%z.CH4 = 0.4; z.NH3 = 0.1; z.Egas = 0.1; z.H2SO4 = 0.1; z.HCN = 0.1; z.AS = 0.1; z.H2 = 0.1; 
-z.H2O = 0.5; z.HCN = 0.5; 
+T_boiling_mixture_assumption = 317.17; %%% need to do bubble point calculation
+
+
 % want to operate the column at atmospheric pressure 
 % pressure = @(temperature) z.CH4*P_sat_CH4(temperature) + z.NH3*P_sat_NH3(temperature) + z.Egas*P_sat_Egas(temperature) + 
 % z.H2SO4*P_sat_H2SO4(temperature) + z.HCN*P_sat_HCN(temperature) + z.AS*P_sat_AS(temperature) + z.H2*P_sat_H2(temperature);
 % formula from solution Separation Technologies, Series 6
 %pressure = @(temperature) z.HCN*P_sat_HCN(temperature) + z.H2O*P_sat_H2O(temperature);
-pressure = 10^5; % Pa
+pressure = strin(9).p; % Pa
 
 
 if strcmp(thermo_model, 'ideal') 
@@ -86,9 +66,9 @@ N_S_min = log(x_LK_D/x_HK_D*x_HK_B/x_LK_B)/log(alpha_mean)-1;
 
 %%% Underwood formula %%%
 options = optimset('Display','off');
-theta0 = 1.1;
+theta0 = 1.1; % just some guess
 theta_sol = fsolve (@(theta) (alpha_mean*z.HCN)/(alpha_mean-theta)+(1*z.H2O)/(1-theta), theta0, options);
-% theta_sol = 1.6183, so between 1 and alpha_mean --> :) 
+% theta_sol between 1 and alpha_mean --> :) 
 RR_min = (alpha_mean*x_LK_D)/(alpha_mean-theta_sol)+(1*x_HK_D)/(1-theta_sol)-1; 
 RR_real = RR_min * 1.5; % heuristic, from Stavros' introduction
 
@@ -140,23 +120,17 @@ B = F-D;
 
 
 
-%%% McCabe-Thiele Diagram
-x_plot = 0:0.01:1;
-y_plot = x_plot; 
-%y_eq = 
-
-
 
 %%% Cost condenser & reboiler 
 % neglecting water in distillate: 
 deltaH_HCN = 25220; % Enthalpy of vaporization of HCN at BP, [J/mol], cmpin.Hv(6)
 Q_cond = deltaH_HCN*D; % Heat duty condenser [J/s]
-cp_coefficients_cooling_water = [34.51; -0.01; 4.686e-5; -3.781e-8; 1.173e-11]; % [cmpin.para(1), cmpin.parb(1), cmpin.parc(1), cmpin.pard(1), cmpin.pare(1)]
+cp_coefficients_cooling_water = [cmpin(1).para, cmpin(1).parb, cmpin(1).parc, cmpin(1).pard, cmpin(1).pare];
 T_cooling_water=15+273.15; % assumed temperature of available cooling water, [K]
 cP_cooling_water = @(temperature) heat_capacity(temperature, cp_coefficients_cooling_water);
 cooling_water_mass_flow = Q_cond*MW_H2O/(cP_cooling_water((T_boiling_HCN+T_cooling_water)/2)*(T_boiling_HCN-T_cooling_water)); % cooling water mass flow [kg/s], evaluating cp_cooling_water at average temperature (cp of H2O doesn't change much in the region in question anyways)
 
-
+test = 1;
 
 
 
