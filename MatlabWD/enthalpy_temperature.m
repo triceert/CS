a47c7b99b9417a99e04975302fd52b615c560db7
Pauline Oeuvray <poeuvray@student.ethz.ch>
2018-04-25 09:46:17 +0200

    Function verbessert

--- MatlabWD/enthalpy_temperature.m
+++ MatlabWD/enthalpy_temperature.m
@@ -1,11 +1,25 @@
-function [HT] = enthalpy_temperature(T,a,b1)
+function [HT] = enthalpy_temperature(T,cmp,unt)
 % - Calculates the enthalpy of a GASEOUS compound with given parameters as a function of the temperature
 %
-% INPUT: a = coefficients a0,a1,a2,a3,a4 from the NASA as 1x5 or 5x1 vector
-%        b1 = coefficient b1 from the NASA
-%        T = Temperature [K]
+% INPUT: T = Temperature [K]
+%        cmp = compound struct     
+%        unt = unit struct
+% OUTPUT: enthalpy [J.mol-1]
 
-R = 8.314; % [J.mol-1.K-1]
-HT = RT*(a(1) + a(2)*T/2 + a(3)*T^2/3 + a(4)*T^3/4 + a(5)*T^4/5)+R*b1; % [J.mol-1]
+R=unt(5).idgc; % [J.mol-1.K-1]
+
+boo = (T > 1000);
+
+switch boo
+    case 0 % T <= 1000K
+        a = [cmp(1:6).a0_hcparlow;cmp(1:6).a1_hcparlow;cmp(1:6).a2_hcparlow;cmp(1:6).a3_hcparlow;cmp(1:6).a4_hcparlow];
+        b1 = cmp(1:6).b1_hcparlow;
+    case 1 % T > 1000K
+        a = [cmp(1:6).a0_hcparhigh;cmp(1:6).a1_hcparhigh;cmp(1:6).a2_hcparhigh;cmp(1:6).a3_hcparhigh;cmp(1:6).a4_hcparhigh];
+        b1 = cmp(1:6).b1_hcparhigh;
+    otherwise
+end
+
+HT = RT.*(a(1,:) + a(2,:).*T/2 + a(3,:).*T^2/3 + a(4,:).*T^3/4 + a(5,:).*T^4/5)+R.*b1;
 end
 
