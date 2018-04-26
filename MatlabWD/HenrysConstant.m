51e65f152e53cff8d32f3ea102c5dea365c3d0d7
Pauline Oeuvray <poeuvray@student.ethz.ch>
2018-04-26 14:00:19 +0200

    Correction

--- MatlabWD/HenrysConstant.m
+++ MatlabWD/HenrysConstant.m
@@ -1,20 +1,17 @@
-function [H] = HenrysConstant(T,cmp)
+function [H] = HenrysConstant(T,cmp,n)
 % - Calculates Henry's constant for a given compound at a given temperature
 %
 % INPUT: T = Temperature [K]
 %        cmp = compound struct     
-%        unt = unit struct
+%        n = compound number
 % OUTPUT: H: Henry's constant for the component [Pa]
 
 
-%Assign Vectors from Compound struct
-%   From Nitrogen to Hydrogen Cyanide (identifier 2-6)
-
-kHstd = cmp(2:6).kHstd; %[M/atm]
-deltaHsolR = cmp(2:6).deltaHsolR; %[K]
+kHstd = cmp(n).kHstd; %[M/atm]
+deltaHsolR = cmp(n).deltaHsolR; %[K]
 Tstd = 298.15; %[K]
 
-kH = kHstd.*exp(-deltaHsolR.*(1/T-1/Tstd)); %[M/atm]
+kH = kHstd*exp(-deltaHsolR*(1/T-1/Tstd)); %[M/atm]
 H = 55.3*101325/kH; %[Pa]
 end
 
