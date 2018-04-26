decb44429b2847b74c937e5fdb769ecf6314b7c2
Pauline Oeuvray <poeuvray@student.ethz.ch>
2018-04-26 13:12:41 +0200

    Does not work properly yet...

--- MatlabWD/ThermalConductivity.m
+++ MatlabWD/ThermalConductivity.m
@@ -4,10 +4,9 @@ function [plambda] = ThermalConductivity(cmp)
 %
 % INPUT: cmp = compound struct     
 %
-% OUTPUT: plambda 17x2 array with polynom of the compound on each line 
+% OUTPUT: plambda 9x2 array with polynom of the compound on each line 
 % Indexes: 1: Water, 2:Nitrogen,3:Methane, 4:Ammonia,5: Hydrogen, 6:Hydrogen cyanide,
-% 7:Sulfuric acid, 8:Ammonium sulfate, 9:Ethane, 10:Propane, 11:Isobutane
-% 12:Butane, 13:Isopentane, 14:Pentane, 15:Hexane, 16:Carbon dioxide, 17:Oxygen
+% 7:Sulfuric acid, 8:Ammonium sulfate, 9:Carbon dioxide
 
 %Gases
 Tgas100 = 100:100:600; % [K]
@@ -21,13 +20,6 @@ lambdagas3 = [cmp(3).lambdagas100,cmp(3).lambdagas200,cmp(3).lambdagas300,cmp(3)
 lambdagas4 = [cmp(4).lambdagas300,cmp(4).lambdagas400,cmp(4).lambdagas500,cmp(4).lambdagas600];
 lambdagas5 = [cmp(5).lambdagas100,cmp(5).lambdagas200,cmp(5).lambdagas300,cmp(5).lambdagas400,cmp(5).lambdagas500,cmp(5).lambdagas600];
 lambdagas9 = [cmp(9).lambdagas200,cmp(9).lambdagas300,cmp(9).lambdagas400,cmp(9).lambdagas500,cmp(9).lambdagas600];
-lambdagas10 = [cmp(10).lambdagas300,cmp(10).lambdagas400,cmp(10).lambdagas500,cmp(10).lambdagas600];
-lambdagas11 = [cmp(11).lambdagas300,cmp(11).lambdagas400,cmp(11).lambdagas500,cmp(11).lambdagas600];
-lambdagas12 = [cmp(12).lambdagas300,cmp(12).lambdagas400,cmp(12).lambdagas500,cmp(12).lambdagas600];
-lambdagas14 = [cmp(14).lambdagas400,cmp(14).lambdagas500,cmp(14).lambdagas600];
-lambdagas15 = [cmp(15).lambdagas400,cmp(15).lambdagas500,cmp(15).lambdagas600];
-lambdagas16 = [cmp(16).lambdagas200,cmp(16).lambdagas300,cmp(16).lambdagas400,cmp(16).lambdagas500,cmp(16).lambdagas600];
-lambdagas17 = [cmp(17).lambdagas100,cmp(17).lambdagas200,cmp(17).lambdagas300,cmp(17).lambdagas400,cmp(17).lambdagas500,cmp(17s).lambdagas600];
 
 %Other compounds
 Tliq = 273.16:25:373.16;
@@ -37,10 +29,9 @@ TLange = 253.16:20:333.16;
 lambdaliq6 = [cmp(6).lambda253,cmp(6).lambda273,cmp(6).lambda293,cmp(6).lambda313,cmp(6).lambda333];
 
 lambdaliq7 = cmp(7).lambdaliq300;
-lambdaliq13 = cmp(13).lambdaliq298;
 
 %Polynomial fitting
-plambda = zeros(17,2);
+plambda = zeros(9,2);
 
 plambda(1,:) = polyfit([Tliq Tgas400],[lambdaliq1 lambdagas1],1);
 plambda(2,:) = polyfit(Tgas100,lambdagas2,1);
@@ -51,12 +42,4 @@ plambda(6,:) = polyfit(TLange,lambdaliq6,1);
 plambda(7,:) = [0,lambdaliq7];
 plambda(8,:) = [NaN,NaN];
 plambda(9,:) = polyfit(Tgas200,lambdagas9,1);
-plambda(10,:) = polyfit(Tgas300,lambdagas10,1);
-plambda(11,:) = polyfit(Tgas300,lambdagas11,1);
-plambda(12,:) = polyfit(Tgas300,lambdagas12,1);
-plambda(13,:) = [0,lambdaliq13];
-plambda(14,:) = polyfit(Tgas400,lambdagas14,1);
-plambda(15,:) = polyfit(Tgas400,lambdagas15,1);
-plambda(16,:) = polyfit(Tgas200,lambdagas16,1);
-plambda(17,:) = polyfit(Tgas100,lambdagas17,1);
 end
