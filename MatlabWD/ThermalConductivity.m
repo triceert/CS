3d6c05de32094bb1129d022372be74b491ec3259
Pauline Oeuvray <poeuvray@student.ethz.ch>
2018-04-26 08:56:24 +0200

    Sieht noch nicht gut aus, aber sollte funktionieren

--- MatlabWD/ThermalConductivity.m
+++ MatlabWD/ThermalConductivity.m
@@ -16,7 +16,7 @@ Tgas200 = 200:100:600; % [K]
 Tgas300 = 300:100:600; % [K]
 Tgas400 = 400:100:600; % [K]
 
-lambdagas1 = [cmp(1).lambdagas300,cmp(1).lambdagas400,cmp(1).lambdagas500,cmp(1).lambdagas600];
+lambdagas1 = [cmp(1).lambdagas400,cmp(1).lambdagas500,cmp(1).lambdagas600];
 lambdagas2 = [cmp(2).lambdagas100,cmp(2).lambdagas200,cmp(2).lambdagas300,cmp(2).lambdagas400,cmp(2).lambdagas500,cmp(2).lambdagas600];
 lambdagas3 = [cmp(3).lambdagas100,cmp(3).lambdagas200,cmp(3).lambdagas300,cmp(3).lambdagas400,cmp(3).lambdagas500,cmp(3).lambdagas600];
 lambdagas4 = [cmp(4).lambdagas300,cmp(4).lambdagas400,cmp(4).lambdagas500,cmp(4).lambdagas600];
@@ -37,10 +37,25 @@ lambdaliq1 = [cmp(1).lambdaliq273,cmp(1).lambdaliq298,cmp(1).lambdaliq323,cmp(1)
 TLange = 253.16:20:333.16;
 lambdaliq6 = [cmp(6).lambda253,cmp(6).lambda273,cmp(6).lambda293,cmp(6).lambda313,cmp(6).lambda333];
 
-p2 = polyfit(Tgas100,lambdagas2,1);
-p3 = polyfit(Tgas100,lambdagas3,1);
-p4 = polyfit(Tgas300,lambdagas4,1);
+lambdaliq7 = cmp(7).lambdaliq300;
+lambdaliq13 = cmp(13).lambdaliq298;
 
-%p = polyfit(T,lambda,1)
-
+p = zeros(17,2);
+p(1,:) = polyfit([Tliq Tgas400],[lambdaliq1 lambdagas1],1);
+p(2,:) = polyfit(Tgas100,lambdagas2,1);
+p(3,:) = polyfit(Tgas100,lambdagas3,1);
+p(4,:) = polyfit(Tgas300,lambdagas4,1);
+p(5,:) = polyfit(Tgas100,lambdagas5,1);
+p(6,:) = polyfit(TLange,lambdaliq6,1);
+p(7,:) = [0,lambdaliq7];
+p(8,:) = [NaN,NaN];
+p(9,:) = polyfit(Tgas200,lambdagas9,1);
+p(10,:) = polyfit(Tgas300,lambdagas10,1);
+p(11,:) = polyfit(Tgas300,lambdagas11,1);
+p(12,:) = polyfit(Tgas300,lambdagas12,1);
+p(13,:) = [0,lambdaliq13];
+p(14,:) = polyfit(Tgas400,lambdagas14,1);
+p(15,:) = polyfit(Tgas400,lambdagas15,1);
+p(16,:) = polyfit(Tgas200,lambdagas16,1);
+p(17,:) = polyfit(Tgas100,lambdagas17,1);
 end
