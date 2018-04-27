0cbd2b654389c20348b590cd5248a89a1bcb32a8
Pauline Oeuvray <poeuvray@student.ethz.ch>
2018-04-27 12:20:18 +0200

    Corrections of r_reactor and D_reactor

--- MatlabWD/HeatTransferCoefficient.m
+++ MatlabWD/HeatTransferCoefficient.m
@@ -11,14 +11,15 @@ function [U] = HeatTransferCoefficient(cmp,unt,p,T,F,cp,Z)
 R=unt(5).idgc;         %[kg.m2.s-2.mol-1.K-1]
 MW_in = extractfield(cmp(2:6),'MW')';
 F_tot = sum(F);
-D_reactor = unt(1).rad;
+r_reactor = unt(1).rad;
+D_reactor = 2*r_reactor;
 L = unt(1).h;
 
 %Wall thermal diffusivity
 
-lambdaWall = cmp(10).lambda; %[m]
+lambdaWall = cmp(10).lambda; %[W.m-1.K-1]
 deltaWall = unt(1).deltaWall; %[m]
-alphaWall = lambdaWall/deltaWall;
+alphaWall = lambdaWall/deltaWall; %[W.m-2.K-1]
 
 %Inside the reactor
 
@@ -30,7 +31,7 @@ rho_mix_in = F_tot*Z*R*T/(p*sum(F.*MW_in));
 Pr_in = Prandtl(cp_mix_in,mu_mix_in,lambda_mix_in);
 
 Q_in = F_tot*Z*R*T/p;
-A_in = pi*D_reactor^2;
+A_in = pi*r_reactor^2;
 nu_mix_in = mu_mix_in/rho_mix_in;
 
 Re_in = Reynolds(Q_in,D_reactor,A_in,nu_mix_in);
@@ -58,7 +59,7 @@ Re_out = unt(1).Reout; %turbulent
 
 Nu_out = Nusselt_out(Re_out,Pr_out);
 
-V_tube = L*pi*(D_reactor/2 + deltaWall)^2;
+V_tube = L*pi*(r_reactor + deltaWall)^2;
 A_tube = L*pi*(D_reactor + 2*deltaWall);
 tube_char_length = V_tube/A_tube;
 
