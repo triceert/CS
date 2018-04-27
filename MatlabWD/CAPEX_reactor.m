e3cb343919208906ef75b1b09391259822cdb675
Pauline Oeuvray <poeuvray@student.ethz.ch>
2018-04-27 17:29:16 +0200

    Revert "Revert "CAPEX reactor done""
    This reverts commit d7e54cb2d030b314f707d43bf8a19465e4bfb6fd.

--- MatlabWD/CAPEX_reactor.m
+++ MatlabWD/CAPEX_reactor.m
@@ -1,9 +1,9 @@
-function [priceUSdollars] = CAPEX_reactor(number_of_tubes,unt)
-% INPUT: number of tubes in the reactor    
-%        unt = unit struct
+function [CAPEX_reactor_priceUSdollars] = CAPEX_reactor(unt)
+% INPUT: unt = unit struct
 % OUTPUT: price of the reactor in US$
 
 price_tube = unt(1).price_tube;
-priceUSdollars = price_tube*number_of_tubes;
+number_of_tubes = unt(1).N_tubes;
+CAPEX_reactor_priceUSdollars = price_tube*number_of_tubes;
 end
 
