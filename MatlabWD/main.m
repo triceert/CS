c0e28f454ea147fa96b62b0968be3aeb2575a298
Tobias Seidler <tseidler@student.ethz.ch>
2018-04-30 13:00:11 +0200

    Stored some usefulf values from optimization

--- MatlabWD/main.m
+++ MatlabWD/main.m
@@ -110,21 +110,21 @@ function [cmp, unt, str]=calculator(cmp,unt,str)
 
 
 %MODELLING PARAMETERS
-unt(1).cocross=1;           % 0 cross 1 co 
+unt(1).cocross=0;           % 0 cross 1 co 
                                     %Heating Medium Flow rate, only usefull when co-current flow
-        str(4).G=0.06;               %in flow heating medium per fucking tube(ignored if cross heated)
-unt(1).ideal_real=0;        %ideal gas 0, peng robinson 1
-unt(1).nrow=6;             %number reactr elements in row
+str(4).G=0.05;               %in flow heating medium per fucking tube(ignored if cross heated)
+unt(1).ideal_real=1;        %ideal gas 0, peng robinson 1
+unt(1).nrow=15;             %number reactr elements in row
 
     
 
 %THERMO
-str(1).p=101325;   %feed pressure
+str(1).p=105325;   %feed pressure
 str(1).T=800;       %feed temperature
 str(2).T=1600;      %touter 
 
 %FEEDS
-str(1).FCH4=0.018; %absolute feed ch4 per single tube mol s-1   
+str(1).FCH4=0.014; %absolute feed ch4 per single tube mol s-1   
 str(1).ubsch=1.05;  %überschuss NH3
 
 
