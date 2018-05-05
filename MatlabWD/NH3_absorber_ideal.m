94d3dc49190fd69fa1c3593c12058880f1f8cf1f
Selina Schlotterbeck <selinsch@student.ethz.ch>
2018-05-05 19:11:18 +0200

    fixed temp of stream 5 to 1600K,  calculation for h2so4 concentration added

--- MatlabWD/NH3_absorber_ideal.m
+++ MatlabWD/NH3_absorber_ideal.m
@@ -5,25 +5,20 @@ untout=untin;
 strout=strin;
 %% Defining variables
 
+%Molar flows of gas and liquid inlet streams
 G = strin(5).G;
-%G = 64;        %test value 
-L = 400;
+L = 200;
 strout(7).G = G;
 strout(7).L = L;
 
-%molar fractions gaseous stream
+%molar fractions inlet gas stream
 y_HCN_in = strin(5).yHCN;
 y_NH3_in = strin(5).yNH3;
 y_H2_in = strin(5).yH2;
 y_CH4_in = strin(5).yCH4;
 y_N2_in = strin(5).yN2;
-%y_HCN_in = 0.1706;
-%y_NH3_in = 0.04;
-%y_H2_in = 0.6876;
-%y_CH4_in = 0.073;
-%y_N2_in = 0.0288;
 
-
+%Molar fractions outlet gas stream
 y_out = 1-(y_NH3_in-10^(-4));       %total gas stream out
 y_HCN_out = y_HCN_in/y_out;
 y_NH3_out = 10^(-4);                 %desired concentration
@@ -37,12 +32,14 @@ strout(7).yH2 = y_H2_out;
 strout(7).yCH4 = y_CH4_out;
 strout(7).yN2 = y_N2_out;
 
-%molar fraction liquid stream
+%molar fractions inlet liquid stream
 x_H2SO4_in = (G*y_NH3_in*1.1*0.5)/L;       
 x_H2O_in = 1 - x_H2SO4_in;
 %x_HCN_out = 0;                      %assumption, no physisorption
 n_NH3 = G*(y_NH3_in-y_NH3_out);                 %assumption molar flow = mole fraction
 n_H2SO4 = L*x_H2SO4_in;                %with or without a factor of 1.1?
+
+%molar fractions outlet liquid stream
 n_tot = L*(x_H2O_in + x_H2SO4_in);
 x_H2O_out = x_H2O_in;
 x_H2SO4_out = (n_H2SO4 - 0.5*n_NH3)/n_tot;
@@ -52,38 +49,53 @@ strout(6).xH2O = x_H2O_out;
 strout(6).xH2SO4 = x_H2SO4_out;
 strout(6).xAS = x_ammoniumsulfate_out;
 
+%Molar volumes for inlet stream
+ V_m_h2so4 = 98.08 / 1.84;              %molecular weight divided by density [cm^3/mol]
+ V_m_h2o = 18.01 / 0.9982;
+ 
+%Volume flows of inlet stream
+ V_h2so4 = x_H2SO4_in * L * V_m_h2so4;
+ V_h2o = x_H2O_in * L * V_m_h2o;
+ 
+%Volume percentage of sulfuric acid in the inlet liquid stream
+ conc_H2SO4 = V_h2so4 / (V_h2so4 + V_h2o) *100;         
+
+
 %% Calculation for Heat Exchanger
 
-%T_gas_reactor= 1600;
-T_gas_reactor = strin(5).T;                 %temperature of inlet gas stream
+T_gas_reactor= 1600;
+%T_gas_reactor = strin(5).T          %temperature of inlet gas stream
 T1_gas = 100+273;                    %desired value
 
-
+%Enthalpy difference of stream 5 for cooling down to 373K
 deltaH_cooling_HCN = enthalpy_temperature(T_gas_reactor, cmpin, untin, 6) - enthalpy_temperature(T1_gas, cmpin, untin, 6);
 deltaH_cooling_NH3 = enthalpy_temperature(T_gas_reactor, cmpin, untin, 4) - enthalpy_temperature(T1_gas, cmpin, untin, 4);
 deltaH_cooling_H2 = enthalpy_temperature(T_gas_reactor, cmpin, untin, 5) - enthalpy_temperature(T1_gas, cmpin, untin, 5);
 deltaH_cooling_CH4 = enthalpy_temperature(T_gas_reactor, cmpin, untin, 3) - enthalpy_temperature(T1_gas, cmpin, untin, 3);
 deltaH_cooling_N2 = enthalpy_temperature(T_gas_reactor, cmpin, untin, 2) - enthalpy_temperature(T1_gas, cmpin, untin, 2);
 
+%Heat flow of cooling down stream 5
 Q_mixture = G * (deltaH_cooling_HCN*y_HCN_in + ...
                  deltaH_cooling_NH3*y_NH3_in +...
                  deltaH_cooling_H2*y_H2_in + ...
                  deltaH_cooling_CH4*y_CH4_in + ...
                  deltaH_cooling_N2*y_N2_in);
 
+%Specifications for counter-current flowing cooling water
 T_H2O_in = 15+273;
 T_H2O_out = 90 + 273;             
 deltaT1 = T1_gas - T_H2O_out;
 deltaT2 = T1_gas - T_H2O_in;
              
 LMTD = (deltaT1 - deltaT2)/(log(deltaT1/deltaT2));
-             
+
+%Area of heat exchanger, needed for Capex of heat exchanger
 A_exchanger = abs(Q_mixture) / (700*LMTD);      %Q was negative, therefore a negative area was produced
                  
 
 
 
-%% First assumption: Physisorption neglected since chemisorption is mostly more effective, furthermore chemisorption is instantaneous and irreversible
+%% Assumption: Physisorption neglected since chemisorption is mostly more effective, furthermore chemisorption is instantaneous and irreversible
 
 % 2 NH3 + H2SO4 --> (NH4)2SO4               %reaction equation for absorption
 % --> n_NH3 = 0.5 n_ammoniumsulfate         %moles
@@ -114,97 +126,91 @@ hf_ammoniumsulfate = @(T) cmpin(8).deltaHf + cmpin(8).cp*(T-293);
 %reaction enthalpy for the absorption of ammonia
 h_rxn = 275020;                 %[J/mol]
 
-
+%Energies of the streams calculated with the enthalpies
 E_gas_in = G.*(y_HCN_in.*(hf_hcn) + y_NH3_in.*(hf_nh3) + y_H2_in.*(hf_h2) + y_CH4_in.*(hf_ch4) + y_N2_in.*(hf_n2));
 E_liquid_in = L.*(x_H2SO4_in*cmpin(7).deltaHf + x_H2O_in*cmpin(1).deltaHf);                 %no variation for enthalpies needed since the liquid has a temperature of 20 �C
 E_gas_out = @(T) G.*(y_HCN_out*hf_hcn_out(T) + y_NH3_out*hf_nh3_out(T) + y_H2_out*hf_h2_out(T) + y_CH4_out*hf_ch4_out(T)+ y_N2_out*hf_n2_out(T));
 E_liquid_out = @(T) L.*(x_H2SO4_out*hf_h2so4_out(T) + x_H2O_out*hf_h2o_out(T)+ x_ammoniumsulfate_out*hf_ammoniumsulfate(T));
 E_rxn = G*h_rxn*(y_NH3_in - y_NH3_out)/2;
 
-
-
-
+%Calculation of outlet temperature with energy balance
 energy_balance = @(T) E_gas_in + E_liquid_in - E_gas_out(T) - E_liquid_out(T) + E_rxn;
-
-
 
 options = optimset('Display','off');
 T = fsolve(energy_balance, 300,options);
 T_celsius = T-273;
-%T=40+273;
 
 strout(6).T = T;
 strout(7).T = T;
 
-%% Different approach
-
 
 %% Calculation of the height of the column with HTU and NTU
 
-%G = str(5).G;          % gas flow
 velocity = 1;           %[m/s]
-dia = diameter_calc2(velocity, G, y_H2_in, y_N2_in, y_HCN_in, y_CH4_in, y_NH3_in);                %randomly set diameter
+dia = diameter_calc2(velocity, G, y_H2_in, y_N2_in, y_HCN_in, y_CH4_in, y_NH3_in);                
 r = dia/2;              %radius of column
 A = r^2*pi;             %cross section of column
 p = 101325;             %pressure in Pascal
-%P_bar = 1;              % pressure in bar
 a = 190;                %[m^2/m^3] specific surface of packing
 ap = 492;               %[m^(-1)] Packing factor (dry)
 dp = 0.025;             %[m] nominal diameter
 R = 8.314472;           %universal gas constant [J/mol*K]
 
-
-
-
-MW_NH3 = 17;            %molecular weights [g/mol]
+%Molecular weights [g/mol]
+MW_NH3 = 17;            
 MW_H2 = 2;
 MW_HCN = 27;
 MW_N2 = 28;
 MW_CH4 = 16;
 G_m = G*(y_NH3_in*MW_NH3 + y_H2_in*MW_H2 + y_HCN_in*MW_HCN + y_N2_in*MW_N2 + y_CH4_in*MW_CH4)/1000;         %[kg/s]
 
-%assumption made that gaseous phase consists only of H2 and HCN
-mu_g = mu_calc2(T);     %[kg/(m*s)]
-rho = rho_calc(T, y_HCN_in, y_H2_in, y_NH3_in, y_CH4_in, y_N2_in )/1000;      %[g/m^3]
-%nu_g = mu_g/rho;
-%Q = G*R*T/P_bar;
 
+%Calculation for Diffusivity
 sum_V_H2 = 7.07;
 sum_V_NH3 = 14.9;
 D = 10^(-3)*T^1.75*(1/MW_H2 + 1/MW_NH3)^0.5/(((sum_V_H2^(1/3))+ (sum_V_NH3^(1/3)))^2)*10^(-4);          %[m^2/s]Frage mit 10^-4 als Faktor oder nicht?
 
+%Calculation of Schmidt number
+%assumption made that gaseous phase consists only of H2 and HCN
+mu_g = mu_calc2(T);                                                             %[kg/(m*s)]
+rho = rho_calc(T, y_HCN_in, y_H2_in, y_NH3_in, y_CH4_in, y_N2_in )/1000;        %[g/m^3]
 Sc = mu_g/(rho*D);
-%Sc = nu_g/D;
 
-
+%Calculation of mass transfer coefficient for gas 
 KG = 5.23*ap*D/(R*T) * (G_m/(ap*mu_g))^0.7 * Sc^(1/3) * (ap*dp)^(-2)*p;
-%KG = 5.23*ap*D/R/T*(((Q/A)/(ap*nu_g))^0.7)*Sc^(1/3)*(ap*dp)^(-2)*p;
+
 NTU = log(y_NH3_in/y_NH3_out);      %Colburn equation
 HTU = G/(KG*a*A);
-flow_ratio = L/G;
 
+
+
+%Calculation of height und volume of column with consideration of free
+%volume share in the column
 Z = NTU*HTU;
-V_column = (dia/2)^2*pi*Z;
+free_vol = 0.74;                            %free volume share in the column
+V_column = (dia/2)^2*pi*Z / free_vol;
+dia_true = (V_column /(pi*Z))^0.5 *2;       %diameter with the consideration of free volume share in column
+
+%Calculating ratios, ratio of height to diameter should be between 5 and 15
+flow_ratio = L/G;
+ratio = Z/dia_true;
 
 
-
-
-ratio = Z/dia;
-
-[opex_H2O, opex_H2SO4, opex_wasterwater, opex_tot] = opex_calc(G, L,  x_H2O_in, x_H2SO4_in, x_H2O_out, x_H2SO4_out, x_ammoniumsulfate_out);
-capex_heatexchanger = A_exchanger^0.65 * 25000;        
+%Calculation of Opex
+[opex_H2O, opex_H2SO4, opex_wasterwater, opex_tot] = opex_calc(G, L,  x_H2O_in, x_H2SO4_in, x_H2O_out, x_H2SO4_out, x_ammoniumsulfate_out);      
 opex_tot2 = opex_tot;
-%assigning values to get rid of warnings
-opex_H2O=opex_H2O;
-opex_H2SO4 = opex_H2SO4;
-opex_wastewater = opex_wasterwater;
+    %assigning values to get rid of warnings
+     opex_H2O=opex_H2O;
+     opex_H2SO4 = opex_H2SO4;
+     opex_wastewater = opex_wasterwater;
 
-
+%Calculation of Capex, including heat exchanger
 capex = capex_calc(Z, dia);
+capex_heatexchanger = A_exchanger^0.65 * 25000;  
 capex_tot = capex_heatexchanger + capex;
 
-%VARS OUT
 
+%VARS OUT
 
 untout(2).htu = HTU;    %height theoretical units NH3 ADSORBER
 untout(2).ntu = NTU;    %number theoritac units
