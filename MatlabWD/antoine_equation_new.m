632477f64bea0825a392178a9165f66cc8af313e
Clemens Isert <clemens.isert@googlemail.com>
2018-05-08 14:28:43 +0200

    some corrections to antoine_equation_new, @Pauline: please check conversion factor

--- MatlabWD/antoine_equation_new.m
+++ MatlabWD/antoine_equation_new.m
@@ -10,13 +10,13 @@ boo1 = (T > 373);
 
 switch boo1
     case 0 %(T <= 100Cels)
-        A = extractfield(cmp(1:7),'antaRT')';
-        B = extractfield(cmp(1:7),'antbRT')';
-        C = extractfield(cmp(1:7),'antcRT')';
+        A = extractfield(cmp(n),'antaRT')';
+        B = extractfield(cmp(n),'antbRT')';
+        C = extractfield(cmp(n),'antcRT')';
     case 1 %(T > 100Cels)
-        A = extractfield(cmp(1:7),'anta100')';
-        B = extractfield(cmp(1:7),'antb100')';
-        C = extractfield(cmp(1:7),'antc100')';
+        A = extractfield(cmp(n),'anta100')';
+        B = extractfield(cmp(n),'antb100')';
+        C = extractfield(cmp(n),'antc100')';
 end
 
 boo2 = (n == 7); %Different source for sulfuric acid
@@ -25,7 +25,7 @@ switch boo2
     case 0 %(n <= 6)
         T = T -273.15; % Formula in Celsius, but all T always handled in K, so need to convert
         P_sat_unconverted = 10.^(A-B./(T+C)); %in psi (pounds per square inch)
-        P_sat = P_sat_unconverted * 6894.76; 
+        P_sat = P_sat_unconverted * 6894.76; %%% double check this
     case 1 %(n = 7)
        P_sat = exp(A-B./(T+C)); 
 end
