1e41789238c9edbe1e8e54afa2c8ea9bfb04c493
Pauline Oeuvray <poeuvray@student.ethz.ch>
2018-05-09 14:48:58 +0200

    Minor changes

--- MatlabWD/antoine_equation_new.m
+++ MatlabWD/antoine_equation_new.m
@@ -6,12 +6,12 @@ function [P_sat] = antoine_equation_new(cmp,T,n)
 %        n = compound index
 % OUTPUT: P_sat = saturation pressure [Pa]
 
-A = extractfield(cmp(n),'anta')';
-B = extractfield(cmp(n),'antb')';
-C = extractfield(cmp(n),'antc')';
+A = extractfield(cmp(n),'anta');
+B = extractfield(cmp(n),'antb');
+C = extractfield(cmp(n),'antc');
 
 t = T - 273.15; % Formula in Celsius, but all T always handled in K, so need to convert
-P_sat_unconverted = 10.^(A-B./(t+C)); %in mmHg (pounds per square inch)
+P_sat_unconverted = 10.^(A-B./(t+C)); %in mmHg
 P_sat = P_sat_unconverted * 133.322; %%% convert to Pa
 
 end
